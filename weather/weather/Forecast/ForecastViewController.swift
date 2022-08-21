52244d4de5a52e7ef40d2d6930b4cc43a9739754
Peter <plarochkin@gmail.com>
2022-08-22 00:08:01 +0900

    increment all: add charts and switch animation between tableView

--- weather/weather/Forecast/ForecastViewController.swift
+++ weather/weather/Forecast/ForecastViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
+import Charts
 
 final class ForecastViewController: UIViewController {
     
+    lazy var chartView: LineChartView = {
+        let chartView = LineChartView()
+        
+        chartView.translatesAutoresizingMaskIntoConstraints = false
+        chartView.backgroundColor = .systemGray6
+        chartView.isHidden = false
+        chartView.alpha = 1
+        chartView.layer.cornerRadius = 10
+        chartView.clipsToBounds = true
+        return chartView
+    }()
+    
     let periodSegmentControl: UISegmentedControl = {
         let segmentedControl = UISegmentedControl(items: ["День", "Неделя", "Месяц"])
+        segmentedControl.selectedSegmentIndex = 2
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         return segmentedControl
     }()
     
     let changeButton: UIButton = {
         let button = UIButton()
-        button.addTarget(self, action: #selector(changeAppearence(_:)),
-                         for: .touchUpInside)
+        button.addTarget(
+            self,
+            action: #selector(buttonDecrease(_:)),
+            for: .allTouchEvents)
+        button.addTarget(
+            self,
+            action: #selector(buttonIncreaseOrRemain(_:)),
+            for: [.touchUpInside, .touchUpOutside, .touchCancel])
+        button.addTarget(
+            self,
+            action: #selector(buttonPressed(_:)),
+            for: [.touchUpInside])
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 10
+        button.isEnabled = true
+        button.backgroundColor = .systemGray5
+        button.setTitle("Графиком", for: .normal)
+        button.setTitleColor(.black, for: .normal)
         return button
     }()
     
     @objc
-    private func changeAppearence(_ button: UIButton) { }
+    private func buttonDecrease(_ button: UIButton) {
+        UIView.animate(withDuration: 0.07){
+            button.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }
+    }
+    
+    @objc
+    private func buttonIncreaseOrRemain(_ button: UIButton) {
+        UIView.animate(withDuration: 0.07){
+            button.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+    }
+    
+    @objc
+    private func buttonPressed(_ button: UIButton) {
+        debugPrint("buttonPressed")
+        UIView.animate(
+            withDuration: 0.3,
+            animations: {
+                swap(&self.forecastTableView.alpha, &self.chartView.alpha)
+            },
+            completion:  { _ in
+                self.forecastTableView.isHidden = !self.forecastTableView.isHidden
+                self.chartView.isHidden = !self.chartView.isHidden
+            }
+        )
+    }
     
     lazy var forecastTableView: UITableView = {
         let tableView = UITableView()
@@ -33,6 +89,8 @@ final class ForecastViewController: UIViewController {
         tableView.register(
             ForecastTableViewCell.self,
             forCellReuseIdentifier: "ForecastTableViewCell")
+        tableView.isHidden = true
+        tableView.alpha = 0
         return tableView
     }()
     
@@ -72,12 +130,26 @@ final class ForecastViewController: UIViewController {
                 constant: -8)
         ]
         constraints += [
+            chartView.topAnchor.constraint(
+                equalTo: titleLabel.bottomAnchor),
+            chartView.leftAnchor.constraint(
+                equalTo: self.view.safeAreaLayoutGuide.leftAnchor,
+                constant: 8),
+            chartView.rightAnchor.constraint(
+                equalTo: self.view.safeAreaLayoutGuide.rightAnchor,
+                constant: -8),
+            chartView.bottomAnchor.constraint(
+                equalTo: changeButton.topAnchor,
+                constant: -8)
+        ]
+        constraints += [
             changeButton.leftAnchor.constraint(
                 equalTo: forecastTableView.leftAnchor),
             changeButton.rightAnchor.constraint(
                 equalTo: forecastTableView.rightAnchor),
             changeButton.heightAnchor.constraint(
-                equalToConstant: Settings.shared.cityFontHeight + 4)
+                equalTo: periodSegmentControl.heightAnchor,
+                multiplier: 1.2)
         ]
         constraints += [
             periodSegmentControl.topAnchor.constraint(
@@ -90,7 +162,7 @@ final class ForecastViewController: UIViewController {
                 equalTo: view.safeAreaLayoutGuide.bottomAnchor,
                 constant: -8),
             periodSegmentControl.heightAnchor.constraint(
-                equalToConstant: Settings.shared.cityFontHeight + 4)
+                equalToConstant: Settings.shared.dateFontHeight + 16)
         ]
         NSLayoutConstraint.activate(constraints)
         
@@ -103,6 +175,8 @@ final class ForecastViewController: UIViewController {
         view.addSubview(titleLabel)
         view.addSubview(changeButton)
         view.addSubview(periodSegmentControl)
+        view.addSubview(chartView)
+        forecastTableView.isHidden = true
         titleLabel.text = "Hello"
         setLayout()
     }
@@ -110,7 +184,7 @@ final class ForecastViewController: UIViewController {
 
 extension ForecastViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        31
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -121,4 +195,8 @@ extension ForecastViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         Settings.shared.cityFontHeight + 4
     }
+}
+
+extension ForecastViewController: ChartViewDelegate {
+    
 }
